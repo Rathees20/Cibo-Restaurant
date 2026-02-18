56e7b6620784a4182f6907e9442c713c9e03dcf5
Rathees Kumar <rathees2006@gmail.com>
2026-02-18 11:07:38 +0530

    images

--- src/components/GallerySection.tsx
+++ src/components/GallerySection.tsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
 import { motion } from "framer-motion";
 
-import dish4 from "@/assets/Icecream.JPG";
-import dish1 from "@/assets/Non Veg Soup.JPG";
+import dish4 from "@/assets/icecream.jpg";
+import dish1 from "@/assets/non-veg-soup.jpg";
 import dish5 from "@/assets/pad-thai-noodles.jpg";
-import dish2 from "@/assets/malai nasi goreng.JPG";
-import dish6 from "@/assets/korean kimichi salad.JPG";
+import dish2 from "@/assets/malai-nasi-goreng.jpg";
+import dish6 from "@/assets/korean-kimichi-salad.jpg";
 import dish3 from "@/assets/panner-lababdar.jpg";
 import desert from "@/assets/desert-delight.jpg";
 import vegSoup from "@/assets/veg-soup.jpg";
